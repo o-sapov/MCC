f5db454556c009405dee9f8562b0ab835b705363
Oleksii Sapov <sapov@gmx.at>
2023-05-03 15:36:48 +0200

    configure base worklfow to bundle packages from node_modules

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './app/assets/scripts/modules.js',
+    output: {
+        path: __dirname + '/docs/assets/scripts',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
